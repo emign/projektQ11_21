8fd24aded5b350efe2db7f0103d19142a5450174
Selemba <sebastian.lempik@gmx.de>
2021-01-19 11:41:23 +0100

    Added basic Scene Structure

---
+++ src/commonMain/kotlin/GameModule.kt
@@ -0,0 +1,20 @@
+import Scenes.*
+import com.soywiz.korge.*
+import com.soywiz.korge.scene.*
+import com.soywiz.korgw.*
+import com.soywiz.korinject.*
+import com.soywiz.korma.geom.*
+import kotlin.reflect.*
+
+object GameModule : Module() {
+
+    override val mainScene: KClass<out Scene> = TestScene::class
+    override val size: SizeInt = SizeInt(1920,1080)
+
+    override suspend fun AsyncInjector.configure() {
+        mapPrototype { TestScene() }
+
+        lastGlobalGameWindow?.fullscreen=true
+    }
+
+}
---
+++ src/commonMain/kotlin/Scenes/Test_Scene.kt
@@ -0,0 +1,14 @@
+package Scenes
+
+import com.soywiz.korev.*
+import com.soywiz.korge.input.*
+import com.soywiz.korge.scene.*
+import com.soywiz.korge.view.*
+import com.soywiz.korim.color.*
+
+class TestScene : Scene() {
+    override suspend fun Container.sceneInit() {
+        solidRect(10,10,color = Colors.GREEN).xy(0,0)
+
+    }
+}
