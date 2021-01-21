970c1de3c583fee381fabe6a0f64c9ad135b8a0a
Selemba <sebastian.lempik@gmx.de>
2021-01-21 11:42:27 +0100

    Server Test

--- src/commonMain/kotlin/Scenes/Test_Scene.kt
+++ src/commonMain/kotlin/Scenes/Test_Scene.kt
@@ -2,15 +2,39 @@ package Scenes
 
 import com.soywiz.korge.scene.*
 import com.soywiz.korge.view.*
-import com.soywiz.korge.view.ktree.readKTree
 import com.soywiz.korim.color.*
-import com.soywiz.korio.file.std.resourcesVfs
+import kotlinx.coroutines.*
+import multiplayer.*
 
 class TestScene : Scene() {
     override suspend fun Container.sceneInit() {
+        solidRect(10,10,color = Colors.GREEN).xy(0,0)
+        //Server Test
+        ///*
+        Multiplayer.startSever()
+        GlobalScope.launch {
+            while (true){
+                println(Multiplayer.read(Strg("")))
+            }
+        }
+        //*/
+        //Client Test
+        /*
+        Multiplayer.startClient(1,"")
+        GlobalScope.launch {
+            Multiplayer.sync(Strg("test"))
+        }
+        */
+    }
+}
 
-        val unserKtree = resourcesVfs["Scene1.ktree"].readKTree(views())
-        addChild(unserKtree)
+class Strg(var d : String):MultiplayerData{
+    override fun toString(): String {
+        return d
+    }
 
+    override fun fromString(str: String?): MultiplayerData {
+        if (!str.isNullOrEmpty())return Strg(str)
+        return Strg("")
     }
 }
