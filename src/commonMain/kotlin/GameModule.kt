db5dc45111b4d78040e217a4a7d7d2687b638560
mihiSg <mxhi@gmx.de>
2021-01-21 16:13:34 +0100

    Added a first version of an enemy with states and a spine animation

--- src/commonMain/kotlin/GameModule.kt
+++ src/commonMain/kotlin/GameModule.kt
@@ -1,6 +1,6 @@
 import Scenes.*
+import com.soywiz.korge.*
 import com.soywiz.korge.scene.*
-import com.soywiz.korge.view.views
 import com.soywiz.korinject.*
 import com.soywiz.korma.geom.*
 import kotlin.reflect.*
@@ -10,12 +10,10 @@ object GameModule : Module() {
     override val mainScene: KClass<out Scene> = TestScene::class
     override val size: SizeInt = SizeInt(1920,1080)
 
-
     override suspend fun AsyncInjector.configure() {
         mapPrototype { TestScene() }
 
-
-
+        lastGlobalGameWindow?.fullscreen=false
     }
 
 }
