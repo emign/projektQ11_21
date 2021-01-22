ac24ecd8e97fe60d6da88f4bb20eb2ce67ebfd08
MihiSg <mxhi@gmx.de>
2021-01-22 21:42:37 +0100

    New update

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,12 +1,3 @@
 import com.soywiz.korge.*
-import com.soywiz.korge.view.*
-import com.soywiz.korim.color.*
-import com.soywiz.korio.file.std.*
-import io.ktor.client.*
-import xmlReader.readPlayer
 
-
-suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"]) {
-    val test = HttpClient()
-
-}
+suspend fun main() = Korge(Korge.Config(module = GameModule))
