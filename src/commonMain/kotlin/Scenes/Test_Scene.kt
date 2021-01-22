f4ee9a93422e6679532000fd20220d449712ef17
mihiSg <mxhi@gmx.de>
2021-01-22 12:57:35 +0100

    Updated state machine. Added TestEntity which uses Spine and States

--- src/commonMain/kotlin/Scenes/Test_Scene.kt
+++ src/commonMain/kotlin/Scenes/Test_Scene.kt
@@ -1,17 +1,15 @@
 package Scenes
 
 //import fsm.Entity
-import com.esotericsoftware.spine.korge.skeletonView
 import com.soywiz.korev.Key
-import com.soywiz.korge.scene.*
+import com.soywiz.korge.scene.Scene
 import com.soywiz.korge.view.*
-import com.soywiz.korim.color.Colors
-import fsm.daTestEnemy
+import fsm.MakeOwnEntityTemplate
 
 class TestScene : Scene() {
     override suspend fun Container.sceneInit() {
 
-        val test = daTestEnemy.build("spineboy-pro.skel", "spineboy-pma.atlas", 1.0f)
+        val test = MakeOwnEntityTemplate.build("spineboy-pro.skel", "spineboy-pma.atlas", 1.0f)
 
         container {
             speed = 1.0
@@ -20,7 +18,8 @@ class TestScene : Scene() {
             addChild(test.modelView)
             //solidRect(10.0, 10.0, Colors.RED).centered
             addUpdater {
-                if(views.keys.justPressed(Key.J)) test.stateSystem.doStateChange(test.jumpState)
+                if (views.keys.justPressed(Key.J)) test.stateManager.doStateChange(test.jumpState)
+                if (views.keys.justPressed(Key.D)) test.stateManager.doStateChange(test.deathState)
             }
         }
 
