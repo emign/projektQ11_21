ab5c8c83605150f3f95604283221b6061a8c79be
Selemba <sebastian.lempik@gmx.de>
2021-01-23 19:03:32 +0100

    Input works

--- src/commonMain/kotlin/Scenes/Test_Scene.kt
+++ src/commonMain/kotlin/Scenes/Test_Scene.kt
@@ -1,20 +1,21 @@
 package Scenes
 
 //import fsm.Entity
-import com.soywiz.korev.GameButton
-import com.soywiz.korev.Key
-import com.soywiz.korge.ext.swf.SWFExportConfig
-import com.soywiz.korge.ext.swf.readSWF
-import com.soywiz.korge.input.onClick
-import com.soywiz.korge.scene.Scene
+import com.soywiz.korge.input.*
+import com.soywiz.korge.scene.*
 import com.soywiz.korge.view.*
-import com.soywiz.korim.format.readBitmap
-import com.soywiz.korim.vector.ShapeRasterizerMethod
-import com.soywiz.korio.file.std.resourcesVfs
-import fsm.MakeOwnEntityTemplate
+import com.soywiz.korim.format.*
+import com.soywiz.korio.file.std.*
+import eventBus.*
+import eventBus.Input
+import fsm.*
 
 class TestScene : Scene() {
     override suspend fun Container.sceneInit() {
+
+        val bus = EventBus(this@TestScene)
+
+        addComponent(Input(this,bus))
 
         val background = Image(resourcesVfs["spineboy-pma.png"].readBitmap()).apply {
             //xy(0,0)
@@ -30,10 +31,6 @@ class TestScene : Scene() {
 
         repeat(50) {
 
-            this += resourcesVfs["dog.swf"].readSWF(views).createMainTimeLine().apply {
-                xy((0..1000).random(), (0..800).random())
-            }
-
             val testEntity = MakeOwnEntityTemplate.build("spineboy-pro.skel", "spineboy-pma.atlas", 1.0f)
 
             container {
@@ -42,16 +39,11 @@ class TestScene : Scene() {
                 position(400, 800)
                 addChild(testEntity.modelView)
                 //solidRect(10.0, 10.0, Colors.RED).centered
-                addUpdater {
-                    if (views.keys.justPressed(Key.J)) testEntity.stateManager.doStateChange(testEntity.jumpState)
-                    if (views.keys.justPressed(Key.D)) testEntity.stateManager.doStateChange(testEntity.deathState)
-                    if (views.keys.justPressed(Key.RIGHT)) testEntity.stateManager.doStateChange(testEntity.runState)
-                    if (views.keys.justReleased(Key.RIGHT)) testEntity.stateManager.doStateChange(testEntity.idleState)
-                }
             }
-
-            solidRect(200, 1080).xy(1900, 0)
-
+            bus.register<IdleEvent>{testEntity.stateManager.doStateChange(testEntity.idleState)}
+            bus.register<RightEvent>{testEntity.stateManager.doStateChange(testEntity.runState)}
+            bus.register<JumpEvent>{testEntity.stateManager.doStateChange(testEntity.jumpState)}
         }
+        solidRect(200, 1080).xy(1900, 0)
     }
 }
