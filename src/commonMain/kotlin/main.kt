c21015cd7190ba854d1f95772522a011b1413d51
nemig <nicolai.emig@amseegym.de>
2021-01-26 19:39:24 +0100

    Not working

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -4,55 +4,8 @@ import com.soywiz.korim.color.*
 import com.soywiz.korio.file.std.*
 import xmlReader.readPlayer
 
+
 suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"]) {
 
-	//test if the reader works correctly -> it does, all data is correctly read
 
-	val character = resourcesVfs["Characters/Test.xml"].readPlayer()
-	addChild(Image(character.model).apply { scale = 0.7 })
-
-	println(character.healthpoints)
-	println(character.movementSpeed)
-	println(character.jumpHeight)
-
-	println(character.standardAttack.name)
-	println(character.standardAttack.damage)
-	println(character.standardAttack.blockable)
-	println(character.standardAttack.ranged)
-	println(character.standardAttack.isSuperAttack)
-	println(character.standardAttack.isFinisher)
-	println(character.standardAttack.cooldown)
-	println(character.standardAttack.isReady)
-
-	println(character.specialAttack.name)
-	println(character.specialAttack.damage)
-	println(character.specialAttack.blockable)
-	println(character.specialAttack.ranged)
-	println(character.specialAttack.isSuperAttack)
-	println(character.specialAttack.isFinisher)
-	println(character.specialAttack.cooldown)
-	println(character.specialAttack.isReady)
-
-	println(character.rangedAttack.name)
-	println(character.rangedAttack.damage)
-	println(character.rangedAttack.blockable)
-	println(character.rangedAttack.ranged)
-	println(character.rangedAttack.isSuperAttack)
-	println(character.rangedAttack.isFinisher)
-	println(character.rangedAttack.cooldown)
-	println(character.rangedAttack.isReady)
-
-	println(character.finisherAttack.name)
-	println(character.finisherAttack.damage)
-	println(character.finisherAttack.blockable)
-	println(character.finisherAttack.ranged)
-	println(character.finisherAttack.isSuperAttack)
-	println(character.finisherAttack.isFinisher)
-	println(character.finisherAttack.cooldown)
-	println(character.finisherAttack.isReady)
-
-	character.states.forEach {
-		println(it.name)
-		println(it.duration)
-	}
 }
