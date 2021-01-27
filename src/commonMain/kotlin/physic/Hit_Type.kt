96e3969cfdfb42fee39893b468bb5bc49674f998
Selemba <sebastian.lempik@gmx.de>
2021-01-27 16:26:29 +0100

    Hit_Type converted to Class

--- src/commonMain/kotlin/physic/Hit_Type.kt
+++ src/commonMain/kotlin/physic/Hit_Type.kt
@@ -1,8 +1,14 @@
 package physic
 
-enum class Hit_Type {
-    Player(),
-    Bullet(),
-    Ground(),
-    Platform()
+import character.*
+import com.soywiz.korge.view.*
+
+abstract class Hit_Type() {
+
+    abstract val view : View?
+
+    class Player(override val view: CharacterBase?=null):Hit_Type()
+    class Bullet(override val view: View?=null):Hit_Type()
+    class Ground(override val view: View?=null):Hit_Type()
+    class Platform(override val view: View?=null):Hit_Type()
 }
