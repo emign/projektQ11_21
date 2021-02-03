bd2308ae3478d69c0117fb9c9b093ce6df9017d1
MihiSg <mxhi@gmx.de>
2021-02-03 20:50:15 +0100

    Added full documentation for the physics. created a module for easy use in korge and created sample scene. Update

--- src/commonMain/kotlin/GameModule.kt
+++ src/commonMain/kotlin/GameModule.kt
@@ -7,12 +7,13 @@ import kotlin.reflect.*
 
 object GameModule : Module() {
 
-    override val mainScene: KClass<out Scene> = TestScene::class
+    override val mainScene: KClass<out Scene> = Physics_Sample::class
     override val size: SizeInt = SizeInt(1920,1080)
     //override val bgcolor: RGBA = Colors.WHITE
 
     override suspend fun AsyncInjector.configure() {
         mapPrototype { TestScene() }
+        mapPrototype { Physics_Sample() }
 
         lastGlobalGameWindow?.fullscreen=false
     }
