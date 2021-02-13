af4dcb985a905272bef6131f10d05923de423a9c
Selemba <sebastian.lempik@gmx.de>
2021-02-13 18:17:25 +0100

    updated KDOC

--- src/commonMain/kotlin/eventController/EventController.kt
+++ src/commonMain/kotlin/eventController/EventController.kt
@@ -14,7 +14,8 @@ class EventController(private val scope: CoroutineScope) {
     private val bus = GlobalBus()
 
     /**
-     *Sends an Event through the EventController with the [Event] as Parameter
+     * Sends an Event through the EventController with the [Event] as Parameter
+     * @param message is the [Event] that needs to be sent
      */
     fun send(message: Event) {
         scope.launchImmediately {
@@ -24,6 +25,7 @@ class EventController(private val scope: CoroutineScope) {
 
     /**
      * Registers a [handler] (lambda) for a specific [Event]
+     * @param handler is the callback lambda
      */
     fun <T : Event> register(clazz: KClass<out T>, handler: suspend (T) -> Unit) {
         bus.register(clazz, handler)
