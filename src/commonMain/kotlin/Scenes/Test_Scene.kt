8ffd3196b9cc01ae56adee7c470f40d9c3a37c68
Selemba <60011459+Selemba1000@users.noreply.github.com>
2021-02-24 10:43:45 +0100

    InputController hinzugefügt (#30)
    * Keyboard Input Controller
    
    * Keyboard Input Controller
    
    * General InputController rework
    
    * fix MouseInputController to local coords
    
    * small fix
    
    * Added DOC
    
    * Update README.md
    
    Co-authored-by: 2105 <stefan.kaufmann@nodeconsult.de>
    Co-authored-by: MihiSg <67419329+mihisg@users.noreply.github.com>

--- src/commonMain/kotlin/Scenes/Test_Scene.kt
+++ src/commonMain/kotlin/Scenes/Test_Scene.kt
@@ -1,11 +1,25 @@
 package Scenes
 
 //import fsm.Entity
+import com.soywiz.korev.*
+import com.soywiz.korge.input.*
 import com.soywiz.korge.scene.Scene
 import com.soywiz.korge.view.Container
+import eventController.*
+import steuerung.*
+import steuerung.InputController.initGamepadController
+import steuerung.InputController.initMouseController
+import steuerung.gamepad.*
+import steuerung.mouse.MouseEvent
 
 class TestScene : Scene() {
     override suspend fun Container.sceneInit() {
+
+        initMouseController()
+
+        eventController.register<MouseEvent> {
+            println (it.data.mouseX.toString()+","+it.data.mouseY)
+        }
 
         /*val testPlayer = Player.build("Characters/Test.xml", this@TestScene).apply { xy(100, 100); scale = 0.1 }
         val testEnemy = Enemy.build("Characters/Test.xml", this@TestScene).apply { xy(300, 300); scale = 0.1 }
